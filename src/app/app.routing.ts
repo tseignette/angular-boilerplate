75bcc3276aa3b3fa1022441ce2fff36c2675f9f7
Thomas Seignette <tseignette@pm.me>
2020-07-26 09:22:13 +0200

    Routing config done

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [
+    RouterModule
+  ],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      {
+        scrollPositionRestoration: 'enabled'
+      }
+    )
+  ]
 })
 export class AppRoutingModule { }

9f21f8a9eba9787725f1e74cbeb4b84f83f2035f
Thomas Seignette <tseignette@pm.me>
2020-11-24 10:04:24 +0100

    Angular update

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -18,8 +18,7 @@ const routes: Routes = [
   ],
   imports: [
     RouterModule.forRoot(routes, {
-      scrollPositionRestoration: 'enabled',
-      initialNavigation: 'enabled'
+      scrollPositionRestoration: 'enabled'
     })
   ]
 })
